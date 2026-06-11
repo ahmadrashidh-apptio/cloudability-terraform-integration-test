0ba57c764a45335acbe5cbf95be326cc039a7505
ahmadrashidh-apptio <ahmad.rashidh@apptio.com>
2026-06-12 00:11:09 +0100

    updates throughput to 3

--- main.tf
+++ main.tf
@@ -14,6 +14,6 @@ provider "aws" {
 resource "aws_efs_file_system" "efs_example" {
 
   throughput_mode                 = "provisioned"
-  provisioned_throughput_in_mibps = 2
+  provisioned_throughput_in_mibps = 3
 
 }
